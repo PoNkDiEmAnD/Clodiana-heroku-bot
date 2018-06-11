19d6291fd59b1f3ce8f69501696242d7d1024b49
P O N K <40171663+PoNkDiEmAnD@users.noreply.github.com>
2018-06-11 16:16:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'Clod!ping') {
-    	message.reply('Clodism is a religion based on a random imgae found on google woth a dash of memes and a teaspoon of cringe')
+    	message.reply('Clodism is a religion based on a random image found on google with a dash of memes and a teaspoon of cringe')
   	}
 });
 
