31f1a639947a6c4caca62ef1bb145b41199f2286
P O N K <40171663+PoNkDiEmAnD@users.noreply.github.com>
2018-06-11 20:23:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'Clod!clodism') {
+    	message.reply('Clodism is a religion based on a random image found a google with a dash of memes and a teaspoon of cringe');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
 var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./token.json');
