c463f04717866f9e33bcc612d2baba4074465b2e
P O N K <40171663+PoNkDiEmAnD@users.noreply.github.com>
2018-06-11 20:12:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,3 +13,39 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./token.json');
+// logger settings
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // The bot listens for command messages which we set to start with `Clod!`
+    if (message.substring(0, 1) == 'Clod!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !start
+            case 'start':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hi! :3 <@' + userID +'>'
+                });
+            break;
+            // add any other commands here.
+         }
+     }
+});
